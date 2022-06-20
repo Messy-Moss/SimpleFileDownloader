1964b9a0adf02ca0c8a8868e40760267ec65620c
Messy-Moss <test@test.com>
2022-06-20 22:23:45 +0200

    Add project files.

---
+++ Downlaoder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDownloader {
+    public class Downlaoder {
+        private readonly string _url;
+        private readonly string _path;
+        private readonly int _bufferSize = 8192;
+
+        private string _mediaType = string.Empty;
+        private long _contentLength = 0;
+        private string _fileName;
+
+        private HttpClient _client;
+
+        public Downlaoder(string url, string path) {
+            _url = url;
+            _path = path;
+        }
+
+        public async Task Start(Action<double> onDownloadingCallback, Action onFinishedCallback) {
+            _client = new HttpClient();
+
+            await setDownloadMetadata();
+
+            using Stream stream = await _client.GetStreamAsync(_url);
+            using Stream fStream = new FileStream(_path + _fileName, FileMode.Create, FileAccess.Write, FileShare.None, _bufferSize, true);
+
+            byte[] buffer = new byte[_bufferSize];
+            int bytesRead = 0;
+            double downloaded = 0;
+
+            while ((bytesRead = await stream.ReadAsync(buffer, 0, _bufferSize)) > 0) {
+                await fStream.WriteAsync(buffer, 0, bytesRead);
+                downloaded += bytesRead;
+                onDownloadingCallback(downloaded / _contentLength);
+            }
+            onFinishedCallback();
+        }
+
+        private async Task setDownloadMetadata() {
+            var response = await _client.GetAsync(_url, HttpCompletionOption.ResponseHeadersRead);
+
+            _contentLength = (long)response.Content.Headers.ContentLength;
+            _mediaType = response.Content.Headers.ContentType.MediaType.Split('/')[1];
+            _fileName = $"/file.{_mediaType}";
+                
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace FileDownloader {
+    public class Program {
+
+        private static string _url = "";
+
+        static async Task Main(string[] args) {
+
+            if (args.Length == 0) {
+                Console.WriteLine("specify a file url to download");
+                return;
+            }
+
+            string pathToWriteTo = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders", "{374DE290-123F-4565-9164-39C4925E467B}", String.Empty).ToString();
+
+            var downloader = new Downlaoder(_url, pathToWriteTo);
+
+            await downloader.Start((double downloaded) => {
+                Console.WriteLine($"Downloaded: {Math.Round(downloaded * 100)}%");
+            }, () => {
+                Console.WriteLine("Download Finished");
+            });
+        }
+    }
+}
